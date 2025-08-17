9f58e0e7bf7bd93fddb57f32c69bea6485bd7c57
LowRenzo17 <lorenzorenzo1738@gmail.com>
2025-08-17 13:45:50 +0300

    week three assignment

---
+++ wk3.py
@@ -0,0 +1,16 @@
+def large_power(base, exponent):
+    result = base ** exponent
+    if result > 5000:
+        return True
+    else:
+        return False
+print(large_power(2, 13))
+
+
+def divisible_by_ten(num):
+    if num % 10 == 0:
+        return True
+    else:
+        return False
+
+print(divisible_by_ten(20))
